11b6d1115772c54b4d4d01a224eb6c19d9431455
Seunghoon <liante0904@gmail.com>
2024-06-26 14:46:12 +0900

    Change chartPath.

--- chart_handler.py
+++ chart_handler.py
@@ -2,7 +2,7 @@
 from telegram import InputMediaPhoto
 import os
 from stock_search import search_stock
-from chart import draw_chart
+from chart import draw_chart, get_last_date, CHART_DIR
 from recent_searches import save_recent_searches
 
 async def generate_and_send_charts(context, chat_id, stock_list, user_id):
@@ -34,6 +34,29 @@ async def generate_and_send_charts(context, chat_id, stock_list, user_id):
                 save_recent_searches(context.bot_data['recent_searches'])
         else:
             continue  # 검색 결과가 없거나 여러 개인 경우 건너뜁니다.
+
+    if current_group:
+        media_groups.append(current_group)
+
+    for group in media_groups:
+        await context.bot.send_media_group(chat_id=chat_id, media=group)
+    
+    for file in files_to_close:
+        file.close()
+
+async def generate_and_send_charts_from_files(context, chat_id, chart_files):
+    media_groups = []
+    current_group = []
+    files_to_close = []
+
+    for chart_filename in chart_files:
+        file = open(chart_filename, 'rb')
+        files_to_close.append(file)
+        if len(current_group) < 10:
+            current_group.append(InputMediaPhoto(file, filename=chart_filename))
+        else:
+            media_groups.append(current_group)
+            current_group = [InputMediaPhoto(file, filename=chart_filename)]
 
     if current_group:
         media_groups.append(current_group)
