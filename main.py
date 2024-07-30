37779e455484488d151d94bd34f2d9279b33baba
Seunghoon <liante0904@gmail.com>
2024-07-30 13:02:41 +0900

    Edit naver_upjong_list template

--- main.py
+++ main.py
@@ -300,11 +300,15 @@ async def show_upjong_list(update: Update, context: CallbackContext) -> None:
         upjong_map = {i: (업종명, 등락률, 링크) for i, (업종명, 등락률, 링크) in enumerate(upjong_list, 1)}
         
         for i, (업종명, 등락률, _) in upjong_map.items():
-            upjong_message += f"{i}. {업종명} - 등락률: {등락률}\n"
+            # 이스케이프 처리
+            업종명 = 업종명.replace('.', '\\.')
+            등락률 = 등락률.replace('.', '\\.').replace('-', '\\-').replace('+', '\\+')
+            upjong_message += f"{i}\\. *{업종명}*   \\[{등락률}\\]\n"
+            print(upjong_message)
 
-        upjong_message += "\n업종 번호 혹은 업종명(정확하게) 입력하세요."
+        upjong_message += "\n업종 번호 혹은 업종명\\(정확하게\\) 입력하세요\\."
         context.user_data['upjong_map'] = upjong_map  # 업종 맵을 저장하여 나중에 사용할 수 있게 함
-        await context.bot.send_message(chat_id=chat_id, text=upjong_message)
+        await context.bot.send_message(chat_id=chat_id, text=upjong_message, parse_mode='MarkdownV2')
     except Exception as e:
         await context.bot.send_message(chat_id=chat_id, text=f"업종 목록을 가져오는 중 오류가 발생했습니다: {e}")
 
