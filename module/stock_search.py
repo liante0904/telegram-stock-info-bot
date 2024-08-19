29c7d6e9cccacbad5dd878b87903cba5c453d696
Seunghoon <liante0904@gmail.com>
2024-08-19 10:56:54 +0900

    edit module in stock_search. lower upper correction

--- module/stock_search.py
+++ module/stock_search.py
@@ -1,4 +1,5 @@
 import requests
+
 def search_stock(query):
     url = 'https://ac.stock.naver.com/ac'
     params = {
@@ -13,6 +14,9 @@ def search_stock(query):
     # query가 6자리이고 첫 5자리가 모두 숫자인지 확인하는 함수
     def is_query_numeric(query):
         return len(query) == 6 and query[:5].isdigit()
+    
+    # query를 소문자로 변환하여 비교
+    query_lower = query.lower()
     
     # 데이터 항목이 1건이면 필터링 없이 바로 반환
     if len(data['items']) == 1:
@@ -34,17 +38,17 @@ def search_stock(query):
         for item in data['items']
         if item['typeCode'] in ['KOSPI', 'KOSDAQ']
         and not (40000 <= int(item['code'][0:5]) <= 49999 and '스팩' in item['name'])
-        and (item['code'] == query if is_query_numeric(query) else item['name'] == query) # 종목명 혹은 종목코드 필터링
+        and (item['code'] == query if is_query_numeric(query) else item['name'].lower() == query_lower) # 종목명 혹은 종목코드 필터링
     ]
     print(filtered_items)
     return filtered_items
 
-
-
-
 def main():
-    r= search_stock('삼성바이오')
-    print('0===>',r[0])
+    r = search_stock('cj')
+    if r:
+        print('0===>', r[0])
+    else:
+        print('No results found.')
 
 if __name__ == '__main__':
     main()
