d75bb661b39b16c0b58e5d9ab86e006f9b272955
Seunghoon <liante0904@gmail.com>
2024-08-19 10:05:22 +0900

    edit module in stock_search

--- module/stock_search.py
+++ module/stock_search.py
@@ -11,10 +11,20 @@ def search_stock(query):
     print(data)
     
     # query가 6자리이고 첫 5자리가 모두 숫자인지 확인하는 함수
-    # 종목코드로 필터링
     def is_query_numeric(query):
         return len(query) == 6 and query[:5].isdigit()
     
+    # 데이터 항목이 1건이면 필터링 없이 바로 반환
+    if len(data['items']) == 1:
+        # 반환할 항목을 추출하여 리스트로 포장
+        item = data['items'][0]
+        result = [{
+            'name': item['name'],
+            'code': item['code']
+        }]
+        print(result)
+        return result
+
     # 필터링된 결과를 저장할 리스트
     filtered_items = [
         {
@@ -33,7 +43,7 @@ def search_stock(query):
 
 
 def main():
-    r= search_stock('005935')
+    r= search_stock('삼성바이오')
     print('0===>',r[0])
 
 if __name__ == '__main__':
