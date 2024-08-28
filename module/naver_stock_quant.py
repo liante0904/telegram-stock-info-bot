eb8b8c280e838b849b510f834dd9bbc852681eb8
Seunghoon <liante0904@gmail.com>
2024-08-28 17:38:30 +0900

    stock split correction

--- module/naver_stock_quant.py
+++ module/naver_stock_quant.py
@@ -39,12 +39,26 @@ def fetch_stock_yield_by_period(stock_code=None, date=None):
     # 날짜별 가격 가져오기
     def fetch_past_price(days_ago):
         past_date = (datetime.now() - timedelta(days=days_ago)).strftime('%Y%m%d')
-        trend_url = f'https://m.stock.naver.com/api/stock/{stock_code}/trend?pageSize=1&bizdate={past_date}'
-        trend_data = fetch_data(trend_url)
+        original_date = datetime.strptime(past_date, '%Y%m%d')
+        one_week_ago = original_date - timedelta(weeks=1)
+
+        while True:
+            trend_url = f'https://api.stock.naver.com/chart/domestic/item/{stock_code}/day?startDateTime={past_date}0000&endDateTime={past_date}0000'
+            trend_data = fetch_data(trend_url)
+            
+            if trend_data and len(trend_data) > 0:
+                return int(trend_data[0]['closePrice'])
+            
+            # 날짜를 하루 전으로 이동
+            original_date -= timedelta(days=1)
+            past_date = original_date.strftime('%Y%m%d')
+            
+            # 일주일 이내로 제한
+            if original_date < one_week_ago:
+                break
         
-        if trend_data:
-            return int(trend_data[0]['closePrice'].replace(',', ''))
         return None
+
 
     # 수익률 계산
     def calculate_return(past_price):
