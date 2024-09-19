217e84692ceec4998328b6ea6659d557830a3b9b
seunghoon shin <liante0904@gmail.com>
2024-09-19 05:42:07 +0000

    Sync changed files from master to dev

--- module/naver_stock_util.py
+++ module/naver_stock_util.py
@@ -69,7 +69,6 @@ def fetch_stock_yield_by_period(stock_code=None, date=None):
     }
 
     def fetch_data(url):
-        # print(f"Fetching data from: {url}")  # Debugging message
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
             return response.json()
@@ -77,58 +76,58 @@ def fetch_stock_yield_by_period(stock_code=None, date=None):
             print(f"Failed to fetch data: Status code {response.status_code}")
             return None
 
-    # 현재 가격 가져오기
-    basic_info_url = f'https://m.stock.naver.com/api/stock/{stock_code}/basic'
-    basic_data = fetch_data(basic_info_url)
+    # 1년간 가격 데이터를 가져오기
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=365)  # 1년 전 날짜
+    start_date_str = start_date.strftime('%Y%m%d')
+    end_date_str = end_date.strftime('%Y%m%d')
 
-    if not basic_data:
+    trend_url = f'https://api.stock.naver.com/chart/domestic/item/{stock_code}/day?startDateTime={start_date_str}0000&endDateTime={end_date_str}0000'
+    trend_data = fetch_data(trend_url)
+
+    if not trend_data:
         return {}
+    
+    # print(trend_data)
 
-    current_price = int(basic_data['closePrice'].replace(',', ''))
+    # 현재 가격을 trend_data의 마지막 데이터에서 가져오기
+    current_price = int(trend_data[-1]['closePrice'])
 
-    # 날짜별 가격 가져오기
-    def fetch_past_price(days_ago):
-        past_date = (datetime.now() - timedelta(days=days_ago)).strftime('%Y%m%d')
-        original_date = datetime.strptime(past_date, '%Y%m%d')
-        one_week_ago = original_date - timedelta(weeks=1)
-
-        while True:
-            trend_url = f'https://api.stock.naver.com/chart/domestic/item/{stock_code}/day?startDateTime={past_date}0000&endDateTime={past_date}0000'
-            trend_data = fetch_data(trend_url)
-            
-            if trend_data and len(trend_data) > 0:
-                return int(trend_data[0]['closePrice'])
-            
-            # 날짜를 하루 전으로 이동
-            original_date -= timedelta(days=1)
-            past_date = original_date.strftime('%Y%m%d')
-            
-            # 일주일 이내로 제한
-            if original_date < one_week_ago:
-                break
-        
-        return None
-
+    # 가격 데이터를 날짜별로 정리
+    prices = {item['localDate']: int(item['closePrice']) for item in trend_data}
 
     # 수익률 계산
     def calculate_return(past_price):
         if past_price is not None:
             percentage_change = round((current_price / past_price - 1) * 100, 2)
             return "{:.2f}".format(percentage_change)
-        return None
+        return "N/A"
 
-    # 1D, 1M, 6M, YTD, 1Y 수익률 계산
+    # 1D, 1W, 1M, 3M, 6M, YTD, 1Y 수익률 계산
     timeframes = {
         '1D': 1,
         '1W': 7,
         '1M': 30,
-        '3M': 90,        
+        '3M': 90,
         '6M': 180,
         'YTD': (datetime.now() - datetime(datetime.now().year, 1, 1)).days,
         '1Y': 365
     }
 
-    returns = {key: calculate_return(fetch_past_price(days)) for key, days in timeframes.items()}
+    returns = {}
+    end_date = datetime.now()
+    
+    for key, days in timeframes.items():
+        target_date = end_date - timedelta(days=days)
+        target_date_str = target_date.strftime('%Y%m%d')
+        past_price = None
+        
+        for date_str in sorted(prices.keys(), reverse=True):
+            if date_str <= target_date_str:
+                past_price = prices.get(date_str)
+                break
+        
+        returns[key] = calculate_return(past_price)
 
     return returns
 
@@ -248,7 +247,8 @@ def calculate_page_count(requested_count: int, page_size: int = 100) -> int:
 
 def main():
     # r = search_stock_code_mobileAPI('이토추')
-    r = search_stock_code('이토추')
+    # r = search_stock_code('이토추')
+    r = fetch_stock_yield_by_period(stock_code='188260')
     if r:
         print('0===>', r)
         
