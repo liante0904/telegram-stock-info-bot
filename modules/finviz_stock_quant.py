ef77d0adfcadcedb6f0ecda85266b0bde536db4d
Seunghoon Shin <liante0904@gmail.com>
2026-01-15 10:49:01 +0900

    refactor: 통합된 데이터 구조로 ETF 및 비ETF 종목 처리 로직 간소화

--- modules/finviz_stock_quant.py
+++ modules/finviz_stock_quant.py
@@ -25,54 +25,29 @@ def fetch_worldstock_info(stock_code):
 
         # stock_data = stock_fundament.json()
         print(stock_fundament.get('Company'))
-        if stock.isETF:
-            data = {
-                '종목명': stock_fundament.get('Company', 'N/A'),
-                'PER': 'N/A',
-                'fwdPER': 'N/A',
-                'PBR': 'N/A',
-                '배당수익률': stock_fundament.get('Dividend TTM', 'N/A'),
-                '예상배당수익률': 'N/A',
-                'ROE': 'N/A',
-                '현재가': stock_fundament.get('Price', 'N/A'),
-                '전일비': stock_fundament.get('Prev Close', 'N/A'),
-                '등락률': stock_fundament.get('Change', 'N/A'),
-                '비고(메모)': 'N/A',
-                '1D': stock_fundament.get('Change', 'N/A'),
-                '1W': stock_fundament.get('Perf Week', 'N/A'),
-                '1M': stock_fundament.get('Perf Month', 'N/A'),
-                '3M': stock_fundament.get('Perf Quarter', 'N/A'),
-                '6M': stock_fundament.get('Perf Half Y', 'N/A'),
-                'YTD': stock_fundament.get('Perf YTD', 'N/A'),
-                '1Y': stock_fundament.get('Perf Year', 'N/A'),
-                '종목코드': stock.ticker,
-                '네이버url': '',
-                'FinvizUrl': stock.quote_url
-            }
-        else:
-            data = {
-                '종목명': stock_fundament.get('Company', 'N/A'),
-                'PER': stock_fundament.get('P/E', 'N/A'),
-                'fwdPER': stock_fundament.get('Forward P/E', 'N/A'),
-                'PBR': stock_fundament.get('P/B', 'N/A'),
-                '배당수익률': stock_fundament.get('Dividend TTM', 'N/A'),
-                '예상배당수익률': stock_fundament.get('Dividend Est.', 'N/A'),
-                'ROE': stock_fundament.get('ROE', 'N/A'),
-                '현재가': stock_fundament.get('Price', 'N/A'),
-                '전일비': stock_fundament.get('Prev Close', 'N/A'),
-                '등락률': stock_fundament.get('Change', 'N/A'),
-                '비고(메모)': stock_fundament.get('Trades', 'N/A'),
-                '1D': stock_fundament.get('Change', 'N/A'),
-                '1W': stock_fundament.get('Perf Week', 'N/A'),
-                '1M': stock_fundament.get('Perf Month', 'N/A'),
-                '3M': stock_fundament.get('Perf Quarter', 'N/A'),
-                '6M': stock_fundament.get('Perf Half Y', 'N/A'),
-                'YTD': stock_fundament.get('Perf YTD', 'N/A'),
-                '1Y': stock_fundament.get('Perf Year', 'N/A'),
-                '종목코드': stock.ticker,
-                '네이버url': '',
-                'FinvizUrl': stock.quote_url
-            }
+        data = {
+            '종목명': stock_fundament.get('Company', 'N/A'),
+            'PER': stock_fundament.get('P/E', 'N/A'),
+            'fwdPER': stock_fundament.get('Forward P/E', 'N/A'),
+            'PBR': stock_fundament.get('P/B', 'N/A'),
+            '배당수익률': stock_fundament.get('Dividend TTM', 'N/A'),
+            '예상배당수익률': stock_fundament.get('Dividend Est.', 'N/A'),
+            'ROE': stock_fundament.get('ROE', 'N/A'),
+            '현재가': stock_fundament.get('Price', 'N/A'),
+            '전일비': stock_fundament.get('Prev Close', 'N/A'),
+            '등락률': stock_fundament.get('Change', 'N/A'),
+            '비고(메모)': stock_fundament.get('Trades', 'N/A'),
+            '1D': stock_fundament.get('Change', 'N/A'),
+            '1W': stock_fundament.get('Perf Week', 'N/A'),
+            '1M': stock_fundament.get('Perf Month', 'N/A'),
+            '3M': stock_fundament.get('Perf Quarter', 'N/A'),
+            '6M': stock_fundament.get('Perf Half Y', 'N/A'),
+            'YTD': stock_fundament.get('Perf YTD', 'N/A'),
+            '1Y': stock_fundament.get('Perf Year', 'N/A'),
+            '종목코드': stock.ticker,
+            '네이버url': '',
+            'FinvizUrl': stock.quote_url
+        }
 
         print(data)
         return data
